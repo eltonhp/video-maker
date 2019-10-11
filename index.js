d7a0362b89d991e74f149f2711e5ef48317f3b23
eltonhp <elton.hadriel@gmail.com>
2019-10-11 00:19:14 -0300

    feat: inserido controle de image

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const robots = {
     image: require('./robots/image')
 }
 async function start() {
-    robots.input()
+   robots.input()
    await robots.text()
    await robots.image()
    const content = robots.state.load();
-   // console.log(JSON.stringify(content, null, 4))
+  // console.log(JSON.stringify(content, null, 4))
+
+
     console.dir(content, {depth: null})
 }
 start()
