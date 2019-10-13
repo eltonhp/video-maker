68edd2571185ef54218daf41365294de6eb26fb0
eltonhp <elton.hadriel@gmail.com>
2019-10-13 18:33:11 -0300

    feat: inserção de video no youtube

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ const robots = {
     input: require('./robots/input.js'),
     state: require('./robots/state.js'),
     image: require('./robots/image'),
-    video: require('./robots/video')
+    video: require('./robots/video'),
+    youtube: require('./robots/youtube')
 }
 async function start() {
-   robots.input()
-   await robots.text()
-   await robots.image()
-   await robots.video();
-   const content = robots.state.load();
+   // robots.input()
+   // await robots.text()
+   // await robots.image()
+   // await robots.video();
+  //  await robots.youtube();
+ //  const content = robots.state.load();
   // console.log(JSON.stringify(content, null, 4))
 
 
-    console.dir(content, {depth: null})
+   // console.dir(content, {depth: null})
 }
 start()
